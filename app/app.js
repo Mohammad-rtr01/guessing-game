5ca200a2e27b0361f17bccf36cd99ed0de17c913
mohammad rtr <assd13@gmail.com>
2024-06-23 03:07:02 -0700

    compleate project

---
+++ app/app.js
@@ -0,0 +1,21 @@
+
+let userAnswer = prompt('Enter your answer') ;
+
+let answer = Math.floor(Math.random()*100) ;
+
+if(userAnswer < answer){
+alert('your gues is lower than');
+}
+else if(userAnswer > answer){
+
+    alert('your gues is bigger than ');
+}
+
+else{alert('your gues is correct')}
+
+
+
+
+
+
+
